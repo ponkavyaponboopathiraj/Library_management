915237cec781a51f783581f3a5967fc6a8ae1d62
ponkavyaponboopathiraj <ponkavya2002@gmail.com>
2025-10-25 17:16:45 +0530

    Updated print statement in Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main{
   public static void main(String[] args) {
-            System.out.println("Begin the Project Happile");
+     System.out.println("Project begins - updated version!");
+
   }
     }
